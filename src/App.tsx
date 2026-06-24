cf501e7808416a32dd9a668b60b0494f30d928c3
Oliwier Lang <oliwier.lang2009@gmail.com>
2026-06-24 22:41:51 +0200

    MVP

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,91 @@
+import { useState } from "react";
+
 function App() {
-  function sendMessage() {
+  const [date, setDate] = useState<string>("");
+  function sendMessage(id: number) {
+    const content = [
+      "Magda says she loves you!",
+      "Magda says she misses you.",
+      "Magda is hungry!",
+      "Magda has a massive stinky poo poo",
+      "Magda is fed up with lach and her bullshit",
+    ];
     fetch("https://ntfy.sh/notifications_from_m", {
       method: "POST", // PUT works too
-      body: "test",
+      body: content[id],
+    });
+  }
+  function sendDate(date: string) {
+    fetch("https://ntfy.sh/notifications_from_m", {
+      method: "POST", // PUT works too
+      body: `Magda wants to meet on ${date}! `,
     });
   }
   return (
     <>
-      <button onClick={sendMessage}>click me</button>
+      <div className="main">
+        <div className="title">
+          <div className="title-border"></div>
+          <h1>I Love You!</h1>
+          <p>Send me a message!</p>
+        </div>
+        <div className="controls">
+          <div className="date">
+            <h1>Date Picker</h1>
+            <input
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+            <button
+              onClick={() => {
+                sendDate(date);
+              }}
+            >
+              Let's Meet!
+            </button>
+          </div>
+          <div className="vl"></div>
+          <div className="quick">
+            <h1>Quick Messages</h1>
+            <button
+              onClick={() => {
+                sendMessage(0);
+              }}
+            >
+              I Love You!
+            </button>
+            <button
+              onClick={() => {
+                sendMessage(1);
+              }}
+            >
+              I Miss You
+            </button>
+            <button
+              onClick={() => {
+                sendMessage(2);
+              }}
+            >
+              I'm hungry
+            </button>
+            <button
+              onClick={() => {
+                sendMessage(3);
+              }}
+            >
+              I need to shit
+            </button>
+            <button
+              onClick={() => {
+                sendMessage(4);
+              }}
+            >
+              Lach jebana mam kurwa dosc
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
